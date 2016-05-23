f7a81a9e8f4954f7a4dd48de7b3965f4daaefc3d
cvidler <christopher.vidler@dynatrace.com>
2016-05-23 17:04:49 +1000

    add non-matched URL feature

--- parse_urls.sh
+++ parse_urls.sh
@@ -61,6 +61,12 @@ fi
 # clear output file
 echo -n "" > $OUTFILE
 
+tmpfile=`mktemp`
+tmpfile2=`mktemp`
+#echo $tmpfile
+# prep tmpfile
+cp $INFILE $tmpfile
+
 # parse regexinput file.
 cat $REGEXFILE | while read -r regex; do
 
@@ -68,20 +74,37 @@ cat $REGEXFILE | while read -r regex; do
 	echo "Testing URL Definition: $regex" >> $OUTFILE
 
 #extra debug, shows all command lines run
-set -x
+#set -x
 
 	echo "Raw Matches: Count, Line#, RawURL" >> $OUTFILE
 	# perl-ify regex
-	# DC RUM regex syntax is not-strictly enforced, perl requires strict syntax, so we'll fix what we can (typically we have to escape '/' forward-slash characters).
+	# DC RUM regex syntax is not-strictly enforced, perl requires strict syntax, so we'll fix what we can (typically we have to escape '/' forward-slash characters), and add a full string match so we can report them properly.
 	perlregex=^${regex//\//\\/}.*$
-	echo $perlregex
-	#cat $INFILE | gawk -v regexvar="$regex" ' $0 ~ regexvar { i++; printf("%i,%i,%s\n", i,NR,$1) } ' >> $OUTFILE
-	cat $INFILE | perl -ne'++$l && /'$perlregex'/ && ++$i && print "$i,$l,$&\n" ' >> $OUTFILE
+	#echo $perlregex
+	perl -ne'++$l && /'$perlregex'/ && ++$i && print "$i,$l,$&\n" ' $INFILE >> $OUTFILE
 	echo "AMD Output: Count, Hits, ReportedURL" >> $OUTFILE
-	#cat $INFILE | gawk -v regexvar="$regex" ' $0 ~ regexvar { res = gensub(regexvar, "boo \\1:\\2:\\3:\\4:", "g", $0); !a[res]++; i++; printf("%i,%s\n", i, res ); } ' >> $OUTFILE
-	cat $INFILE | perl -ne's/'$perlregex'/\1\2\3\4\5\6\7\8\9/ && print "$1$2$3$4$5$6$7$8$9\n"' | perl -ne'{chop; $u{$_}++} END { print ++$i.",".$u{$_}.",".$_."\n" for keys %u} ' >> $OUTFILE
+	perl -ne's/'$perlregex'/\1\2\3\4\5\6\7\8\9/ && print "$1$2$3$4$5$6$7$8$9\n"' $INFILE | perl -ne'{chop; $u{$_}++} END { print ++$i.",".$u{$_}.",".$_."\n" for keys %u} ' >> $OUTFILE
 
+#set -x
+	# find non-matching lines, process URL list in order (as per URL definitions) remaining URLs move to next regex, remaining ones after all regexs parsed are reported.
+	#wc -l $tmpfile
+	perl -ne'if ($_ !~/'$perlregex'/) { print $_ } ' $tmpfile > $tmpfile2
+	mv $tmpfile2 $tmpfile
+	#wc -l $tmpfile
+#set +x
 
+	#echo ""
 	echo "" >> $OUTFILE
 
 done 
+
+echo "Non-Matching Lines: Count, RawURL" >> $OUTFILE
+perl -ne'!/^[#\s]/ &&  print ++$i.",".$_ ' $tmpfile >> $OUTFILE
+rm -f $tmpfile
+rm -f $tmpfile2
+
+echo "" >> $OUTFILE
+echo "End" >> $OUTFILE
+echo "Finished."
+echo ""
+
